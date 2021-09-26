d2ece358c5f584829dfb42a01820cf82b07c9c42
Hopeuche360 <hopechijuka@gmail.com>
2021-09-26 15:40:19 +0100

    CI_CD

--- src/main/java/com/example/week9task/config/ScheduledTasks.java
+++ src/main/java/com/example/week9task/config/ScheduledTasks.java
@@ -2,13 +2,10 @@ package com.example.week9task.config;
 
 import com.example.week9task.model.User;
 import com.example.week9task.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Configuration
@@ -21,8 +18,6 @@ public class ScheduledTasks {
     public ScheduledTasks(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-//    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
-//    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     @Scheduled(fixedDelay = 180000)
     public void remove() {
